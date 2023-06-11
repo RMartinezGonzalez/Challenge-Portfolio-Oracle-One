f5343aa63a06349c6ea41c8ff25a2bfc1f1b102c
R <ric.martinez.gonzalez@gmail.com>
2023-06-10 22:29:44 -0600

    fixing the 404 error on GitHub Pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./docs/**/*.{html,js}"],
   theme: {
     extend: {},
     colors: {
       'clr-primary-500': 'hsl(166, 82%, 28%)', /* hsl */
       'clr-primary-700': '#2a2e2d',
     
-      'clr-neutral-50': 'HSL(0, 0%, 100%)',
+      'clr-neutral-50': 'hsl(0, 0%, 100%)',
       'clr-neutral-100': 'hsl(173, 72%, 82%)', /* hsl */
       'clr-neutral-200': 'hsl(0, 0%, 8%)', /* hsl */
       'clr-neutral-400': '#1a1a1a',
