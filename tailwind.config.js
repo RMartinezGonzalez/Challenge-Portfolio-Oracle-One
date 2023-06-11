a5b44c11875836c3e77d41d85e841f69990dc1c7
R <ric.martinez.gonzalez@gmail.com>
2023-06-11 00:53:18 -0600

    Añadiendo todo el HTML

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,11 @@ module.exports = {
       'clr-primary-500': 'hsl(166, 82%, 28%)', /* hsl */
       'clr-primary-700': '#2a2e2d',
     
-      'clr-neutral-50': 'hsl(0, 0%, 100%)',
-      'clr-neutral-100': 'hsl(173, 72%, 82%)', /* hsl */
-      'clr-neutral-200': 'hsl(0, 0%, 8%)', /* hsl */
-      'clr-neutral-400': '#1a1a1a',
-      'clr-neutral-600': '#000',
+      'clr-neutral-100': 'hsl(0, 0%, 100%)',
+      'clr-neutral-200': 'hsl(173, 72%, 82%)', /* hsl */
+      'clr-neutral-500': 'hsl(0, 0%, 8%)', /* hsl */
+      'clr-neutral-700': '#1a1a1a',
+      'clr-neutral-900': '#000',
     }
   },
   plugins: [],
