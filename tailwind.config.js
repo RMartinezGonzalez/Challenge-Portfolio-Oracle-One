4433152b630eedb85e04992ab9318703250dacfb
R <ric.martinez.gonzalez@gmail.com>
2023-06-15 22:30:26 -0600

    se agregaron los estilos de la versión mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,10 @@ module.exports = {
       'clr-primary-500': 'hsl(166, 82%, 28%)', /* hsl */
       'clr-primary-700': '#2a2e2d',
     
+      
       'clr-neutral-100': 'hsl(0, 0%, 100%)',
       'clr-neutral-200': 'hsl(173, 72%, 82%)', /* hsl */
+      'clr-neutral-400': 'hsl(184, 48%, 49%)', /* hsl */
       'clr-neutral-500': 'hsl(0, 0%, 8%)', /* hsl */
       'clr-neutral-700': '#1a1a1a',
       'clr-neutral-900': '#000',
