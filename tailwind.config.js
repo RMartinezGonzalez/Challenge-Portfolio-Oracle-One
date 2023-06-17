b416dbb5e2573a75c8e7076d621df8b98487fc9d
R <ric.martinez.gonzalez@gmail.com>
2023-06-16 21:58:42 -0600

    Agregado los estilos de escritorio y se trata de arreglar el error de no carga de css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   theme: {
     extend: {},
     colors: {
+      'clr-primary-400': 'hsl(166, 81%, 39%)', /* hsl */
       'clr-primary-500': 'hsl(166, 82%, 28%)', /* hsl */
       'clr-primary-700': '#2a2e2d',
     
@@ -16,6 +17,22 @@ module.exports = {
       'clr-neutral-500': 'hsl(0, 0%, 8%)', /* hsl */
       'clr-neutral-700': '#1a1a1a',
       'clr-neutral-900': '#000',
+    },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1296px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
     }
   },
   plugins: [],
